7a132e0672df1771a6b950b0c98d4ab5f54a6299
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-03-25 18:45:14 +0500

    Update crowdin-verify.js

--- src/commands/utility/crowdin-verify.js
+++ src/commands/utility/crowdin-verify.js
@@ -62,7 +62,7 @@ module.exports = {
         }
 
         message.channel.startTyping()
-        const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'], })
+        const browser = await puppeteer.launch({ headless: false, args: ['--incognito', '--single-process', '--no-sandbox', '--no-zygote', '--disable-setuid-sandbox'], })
         let page = await browser.newPage()
         await page.setViewport({ width: 1400, height: 900 })
         const profileUrl = `https://crowdin.com/profile/${member.username}`
